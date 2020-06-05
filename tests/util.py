a78caabb902808f969a3b431ec42f7a854899883
Trang Truong <trang.truong@gigacover.com>
2020-06-05 22:39:06 +0700

    move run function to util file

---
+++ tests/util.py
@@ -0,0 +1,18 @@
+import time, os
+
+APP_HOME = os.path.abspath(__file__ + '/../..')
+
+def run_googlesearchdemo(driver, browser_name):
+    driver.get('http://www.google.com')
+
+    # enter search keyword
+    search_input = driver.find_element_by_name('q')
+    search_input.send_keys('360f product')
+    search_input.submit()
+
+    # take screenshot
+    time.sleep(3)
+    driver.save_screenshot(f'{APP_HOME}/screen_shot/{browser_name}.png')
+
+    # the end
+    driver.quit()  # CAUTION: don't forget to call .quit() or you will get timeout from :brs
