8f8ea08ac92294971fef7034d6a781ccfe83d3a6
Trang Truong <trang.truong@gigacover.com>
2020-06-05 22:38:16 +0700

    brs demo on mobile and desktop

--- tests/brs_desktop_demo.py
+++ tests/brs_desktop_demo.py
@@ -1,23 +1,5 @@
-import time, os
 from src.service.webdriver import load_webdriver_mswindows, load_webdriver_macos
-
-APP_HOME = os.path.abspath(__file__ + '/../..')
-
-def run_googlesearchdemo(driver, browser_name):
-    driver.get('http://www.google.com')
-
-    # enter search keyword
-    search_input = driver.find_element_by_name('q')
-    search_input.send_keys('360f product')
-    search_input.submit()
-
-    # take screenshot
-    time.sleep(3)
-    driver.save_screenshot(f'{APP_HOME}/screen_shot/{browser_name}.png')
-
-    # the end
-    driver.quit()  # CAUTION: don't forget to call .quit() or you will get timeout from :brs
-
+from tests.util import run_googlesearchdemo
 
 class Test:
 
---
+++ tests/brs_mobile_demo.py
@@ -0,0 +1,12 @@
+from src.service.appium_webdriver import load_webdriver_android, load_webdriver_ios
+from tests.util import run_googlesearchdemo
+
+class Test:
+
+    def test_IOS(self):
+        wd = load_webdriver_ios(device='iPhone XS', os_version='13', name='IOS test')
+        run_googlesearchdemo(wd, 'IOS')
+
+    def test_android(self):
+        wd = load_webdriver_android(device='Samsung Galaxy S10', os_version='9.0', name='Android test')
+        run_googlesearchdemo(wd, 'Android')
