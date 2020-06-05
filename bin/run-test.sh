bb1e3705f608dd9725baa646273a68ae4007e739
namgivu <namgivu@gmail.com>
2020-06-05 18:53:04 +0700

    adding -n6 to run-test

--- bin/run-test.sh
+++ bin/run-test.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
+docstring='
+ ./bin/run-test.sh      # run normal
+ ./bin/run-test.sh -n6  # run with 6 parallel threads
+'
+
 SH=`cd $(dirname $BASH_SOURCE) && pwd`
 AH=`cd $SH/.. && pwd`
 
 [[ -z $pipenvsync ]] || pipenv sync  # skip package install as default, otherwise must set :pipenvsync param
 
     cd $AH
-        PYTHONPATH=`pwd` pipenv run pytest
+        PYTHONPATH=`pwd` pipenv run pytest $*
     cd - 1>/dev/null
