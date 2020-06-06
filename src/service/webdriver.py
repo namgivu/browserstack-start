8fc4ac23d6d7a0513ab08be8d76d14c7d77fae73
namgivu <namgivu@gmail.com>
2020-06-06 12:39:09 +0700

    tiny code

--- src/service/webdriver.py
+++ src/service/webdriver.py
@@ -6,8 +6,8 @@ APP_HOME = os.path.abspath(__file__ + '/../../..')
 
 #region load .env file
 env_f=f'{APP_HOME}/.env'
-if os.path.isfile(env_f) is False: raise Exception(f'Not found .env at {env_f} - please clone one from {APP_HOME}/.env_vault/.env.xxx')
-else:                              load_dotenv(dotenv_path=env_f, override=True)
+if not os.path.isfile(env_f): raise Exception(f'Not found .env at {env_f} - please clone one from {APP_HOME}/.env_vault/.env.xxx')
+else:                         load_dotenv(dotenv_path=env_f, override=True)
 #endregion
 
 
