471cab79a5b3371a75e8de557d356edaf49fa0a9
namgivu <namgivu@gmail.com>
2020-06-06 12:49:35 +0700

    more tiny reader friendly

--- tests/test_brs_desktop_demo.py
+++ tests/test_brs_desktop_demo.py
@@ -4,26 +4,21 @@ from tests.util import run_googlesearchdemo
 class Test:
 
     def test_IE(self):
-        wd = load_webdriver_win10(browser='IE', browser_version='11', session_name='IE First Test')  # wd aka webdriver
-        run_googlesearchdemo(wd, 'IE')
+        b='IE';        v='11'; sn='IE First Test'           ; wd=load_webdriver_win10(b, v, sn); run_googlesearchdemo(wd, b)  # b aka browser_name, v aka browser_version, sn aka session_name, wd aka webdriver
 
     def test_Edge(self):
-        # TODO why can not run with Edge version 80 81
-        wd = load_webdriver_win10(browser='Edge', browser_version='18', session_name='Edge First Test')
-        run_googlesearchdemo(wd, 'Edge')
+        #TODO why can not run with Edge version 80 81
+        b='Edge';      v='18'; sn='Edge First Test'         ; wd=load_webdriver_win10(b, v, sn); run_googlesearchdemo(wd, b)  # b aka browser_name, v aka browser_version, sn aka session_name, wd aka webdriver
 
     def test_Firefox(self):
-        wd = load_webdriver_win10(browser='Firefox', browser_version='74', session_name='Firefox First Test')
-        run_googlesearchdemo(wd, 'Firefox')
+        b='Firefox';   v='74'; sn='Firefox First Test'      ; wd=load_webdriver_win10(b, v, sn); run_googlesearchdemo(wd, b)  # b aka browser_name, v aka browser_version, sn aka session_name, wd aka webdriver
 
     def test_Chrome(self):
-        wd = load_webdriver_win10(browser='Chrome', browser_version='81', session_name='Chrome First Test')
-        run_googlesearchdemo(wd, 'Chrome')
+        b='Chrome';    v='81'; sn='Chrome First Test'       ; wd=load_webdriver_win10(b, v, sn); run_googlesearchdemo(wd, b)  # b aka browser_name, v aka browser_version, sn aka session_name, wd aka webdriver
 
     def test_Safari(self):
-        wd = load_webdriver_osx(browser='Safari', browser_version='13', session_name='Safari First Test')
-        run_googlesearchdemo(wd, 'Safari')
+        b='Safari';    v='13'; sn='Safari First Test'       ; wd=load_webdriver_osx(b, v, sn); run_googlesearchdemo(wd, b)  # b aka browser_name, v aka browser_version, sn aka session_name, wd aka webdriver
 
     def test_Chrome_OSX(self):
-        wd = load_webdriver_osx(browser='Chrome', browser_version='81', session_name='Chrome_OSX First Test')
-        run_googlesearchdemo(wd, 'Chrome_OSX')
+        b='Chrome';    v='81'; sn='Chrome_OSX First Test'   ; wd=load_webdriver_osx(b, v, sn); run_googlesearchdemo(wd, 'Chrome_OSX')  # b aka browser_name, v aka browser_version, sn aka session_name, wd aka webdriver
+        #                                                                                                               CAUTION: not :b here
