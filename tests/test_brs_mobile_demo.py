1549aa37cd783f48677c20469617c1d1388f01ea
namgivu <namgivu@gmail.com>
2020-06-08 04:43:09 +0700

    clean up comments

--- tests/test_brs_mobile_demo.py
+++ tests/test_brs_mobile_demo.py
@@ -2,10 +2,15 @@ from src.service.appium_webdriver import load_appium_webdriver
 from tests.util import run_googlesearchdemo
 
 class Test:
+    """
+    d    aka  device
+    osv  aka  os_version,
+    sn   aka  session_name
+    """
 
     def test_IOS(self):
-        d='iPhone XS'           ; osv='13'  ; sn='IOS test'     ; wd=load_appium_webdriver(d, osv, sn); run_googlesearchdemo(wd, tag=d)  # d aka device, osv aka os_version, sn aka session_name
+        d='iPhone XS'           ; osv='13'  ; sn='IOS test'     ; wd=load_appium_webdriver(d, osv, sn); run_googlesearchdemo(wd, tag=d)  
 
 
     def test_android(self):
-        d='Samsung Galaxy S10'  ; osv='9.0' ; sn='Android test' ; wd=load_appium_webdriver(d, osv, sn); run_googlesearchdemo(wd, tag=d)  # d aka device, osv aka os_version, sn aka session_name
+        d='Samsung Galaxy S10'  ; osv='9.0' ; sn='Android test' ; wd=load_appium_webdriver(d, osv, sn); run_googlesearchdemo(wd, tag=d)  
