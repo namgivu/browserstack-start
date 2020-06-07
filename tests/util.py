8f3acc078790c739759c2e4c581c726f60daa649
namgivu <namgivu@gmail.com>
2020-06-08 04:28:47 +0700

    use :wd as nn+tr standard var

--- tests/util.py
+++ tests/util.py
@@ -2,17 +2,17 @@ import time, os
 
 APP_HOME = os.path.abspath(__file__ + '/../..')
 
-def run_googlesearchdemo(driver, tag:'run tag used in snapshot filename'):  #TODO remove browser_name   #TODO should we name :driver as :wd ie :webdriver?
-    driver.get('http://www.google.com')
+def run_googlesearchdemo(wd: 'web driver', tag: 'run tag used in snapshot filename'):  #TODO remove browser_name   #TODO should we name :driver as :wd ie :webdriver?
+    wd.get('http://www.google.com')
 
     # enter search keyword
-    search_input = driver.find_element_by_name('q')
+    search_input = wd.find_element_by_name('q')
     search_input.send_keys('360f product')
     search_input.submit()
 
     # take screenshot
     time.sleep(3)
-    driver.save_screenshot(f'{APP_HOME}/screen_shot/{tag}.png')
+    wd.save_screenshot(f'{APP_HOME}/screen_shot/{tag}.png')
 
     # the end
-    driver.quit()  # CAUTION: don't forget to call .quit() or you will get timeout from :brs
+    wd.quit()  # CAUTION: don't forget to call .quit() or you will get timeout from :brs
