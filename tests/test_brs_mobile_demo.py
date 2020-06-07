20270524c043d61bef4176047ea72f6279ae52ce
namgivu <namgivu@gmail.com>
2020-06-08 04:31:16 +0700

    reader friendly

--- tests/test_brs_mobile_demo.py
+++ tests/test_brs_mobile_demo.py
@@ -8,9 +8,5 @@ class Test:
     sn   aka  session_name
     """
 
-    def test_IOS(self):
-        d='iPhone XS'           ; osv='13'  ; sn='IOS test'     ; wd=load_appium_webdriver(d, osv, sn); run_googlesearchdemo(wd, tag=d)  
-
-
-    def test_android(self):
-        d='Samsung Galaxy S10'  ; osv='9.0' ; sn='Android test' ; wd=load_appium_webdriver(d, osv, sn); run_googlesearchdemo(wd, tag=d)  
+    def test_ios(self):     d='iPhone XS'           ; osv='13'  ; sn='IOS test'     ; wd=load_appium_webdriver(d, osv, sn); run_googlesearchdemo(wd, tag=d)
+    def test_android(self): d='Samsung Galaxy S10'  ; osv='9.0' ; sn='Android test' ; wd=load_appium_webdriver(d, osv, sn); run_googlesearchdemo(wd, tag=d)
