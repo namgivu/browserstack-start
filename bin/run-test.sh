96a14793c01a3fc9ca9fec3fd0e4fa2781620e70
namgivu <namgivu@gmail.com>
2020-06-08 14:36:17 +0700

    rerun 2times if fail

--- bin/run-test.sh
+++ bin/run-test.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 docstring='
- ./bin/run-test.sh      # run normal
- ./bin/run-test.sh -n6  # run with 6 parallel threads
+./bin/run-test.sh      # run normal
+./bin/run-test.sh -n6  # run with 6 parallel threads
+
+./bin/run-test.sh -n8 --reruns 2  # run parallel as 8 and retry-max-2-times/each-test-if-fail
 '
 
 SH=`cd $(dirname $BASH_SOURCE) && pwd`
