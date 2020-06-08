ecc52d733e288787099559a88c15f4533d58dcc6
namgivu <namgivu@gmail.com>
2020-06-08 14:36:03 +0700

    spacing

--- tests/test_brs_desktop_demo.py
+++ tests/test_brs_desktop_demo.py
@@ -10,11 +10,11 @@ class Test:
     """
 
     # mswindows
-    def test_Chrome(self):     b='Chrome';    v='81'; sn='Chrome First Test'       ; wd=load_webdriver_win10(b, v, sn)     ; run_googlesearchdemo(wd, tag=f'{b}_win10')
-    def test_Firefox(self):    b='Firefox';   v='74'; sn='Firefox First Test'      ; wd=load_webdriver_win10(b, v, sn)     ; run_googlesearchdemo(wd, tag=f'{b}_win10')
-    def test_IE(self):         b='IE';        v='11'; sn='IE First Test'           ; wd=load_webdriver_win10(b, v, sn)     ; run_googlesearchdemo(wd, tag=f'{b}_win10')
-    def test_Edge(self):       b='Edge';      v='18'; sn='Edge First Test'         ; wd=load_webdriver_win10(b, v, sn)     ; run_googlesearchdemo(wd, tag=f'{b}_win10')  # TODO why can not run with Edge version 80 81
+    def test_Chrome(self):     b='Chrome';    v='81'; sn='Chrome First Test'       ; wd=load_webdriver_win10(b, v, sn)   ; run_googlesearchdemo(wd, tag=f'{b}_win10')
+    def test_Firefox(self):    b='Firefox';   v='74'; sn='Firefox First Test'      ; wd=load_webdriver_win10(b, v, sn)   ; run_googlesearchdemo(wd, tag=f'{b}_win10')
+    def test_IE(self):         b='IE';        v='11'; sn='IE First Test'           ; wd=load_webdriver_win10(b, v, sn)   ; run_googlesearchdemo(wd, tag=f'{b}_win10')
+    def test_Edge(self):       b='Edge';      v='18'; sn='Edge First Test'         ; wd=load_webdriver_win10(b, v, sn)   ; run_googlesearchdemo(wd, tag=f'{b}_win10')  # TODO why can not run with Edge version 80 81
 
     # macos
-    def test_Chrome_OSX(self): b='Chrome';    v='81'; sn='Chrome_OSX First Test'   ; wd=load_webdriver_osx(b, v, sn)       ; run_googlesearchdemo(wd, tag=f'{b}_osx')
-    def test_Safari(self):     b='Safari';    v='13'; sn='Safari First Test'       ; wd=load_webdriver_osx(b, v, sn)       ; run_googlesearchdemo(wd, tag=f'{b}_osx')
+    def test_Chrome_OSX(self): b='Chrome';    v='81'; sn='Chrome_OSX First Test'   ; wd=load_webdriver_osx(b, v, sn)     ; run_googlesearchdemo(wd, tag=f'{b}_osx')
+    def test_Safari(self):     b='Safari';    v='13'; sn='Safari First Test'       ; wd=load_webdriver_osx(b, v, sn)     ; run_googlesearchdemo(wd, tag=f'{b}_osx')
